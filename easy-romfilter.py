eaf4dbb8054bf758e9bd515043cc667dce756a9d
Takiro <takiro-git@gelbfuchs.de>
2014-09-10 08:20:10 +0200

    added a region/language flag for filtering
    region language flag can be used to specify a comma seperated list of Regions or Languages.

--- easy-romfilter.py
+++ easy-romfilter.py
@@ -12,14 +12,27 @@ class RomFilter:
 		self.output_directory = args.output_directory
 		self.verbosity = args.v
 		self.simulate = args.simulate
+		self.extension = args.extension
+		
 		self.fatal = -1
 		self.warn = 0
 		self.info = 1
 		self.debug = 2
 		self.trace = 3
-		self.extension = args.extension
 
-		self.lang = lang = {'G':4.0, 'E':3.0, 'U':2.0, 'J':1.0}
+		# check for user defined region string
+		if args.regions:
+			self.region = {}
+			rtags = args.regions.upper().split(',')
+			l = len(rtags)
+			
+			for k,v in enumerate(rtags):
+				# create score for each region
+				# we assume that regions are orderd descending by priority
+				self.region[v] = float(l-k)
+		else:
+			#default
+			self.region = {'U':4.0, 'UK':3.0, 'E':2.0, 'J':1.0}
 
 	# Suggested verbosity levels
 	# 1 info
@@ -39,17 +52,21 @@ class RomFilter:
 		 #	4	optional: version info 				(\(V([0-9]\.[0-9])\))?
 		 #	5		version number X.X 				([0-9]\.[0-9])
 		 #	6	optional: verfied good dump 		(\[!\])?
-		pattern = r'(.*)\(([G|E|U|J])\).?(\(M[0-9]\))?.?(\(V([0-9]\.[0-9])\))?.?(\[!\])?\.'
-		pattern += self.extension
+		pattern = r'(.*)\(([A-Z]{1,2})\).?(\(M[0-9]\))?.?(\(V([0-9]\.[0-9])\))?.?(\[!\])?\.'
+		pattern += self.extension # TODO Compile the regex in constructor
 		match = re.match(pattern, file_name)
 
 		if match:
-
+			
 			# check language
-			if match.group(2):
-				x = self.lang[match.group(2)]
-			else:
-				x = 0.0
+			if match.group(2): 						# was there a match
+				if match.group(2) in self.region:	# and is the region in our dict
+					x = self.region[match.group(2)]
+				else:
+					return -1.0 # no match
+			else: 
+				x = 0.0 # XXX this still allows roms without language flag
+						# maybe a strict flag would come in handy
 
 			# check for good dump
 			if match.group(6):
@@ -97,11 +114,14 @@ class RomFilter:
 
 		if most_rated != '':
 
+			# FIXME: replace all non alphanumeric characters (use a regex)
+			# this is crap
 			wrongChar= [".", "-", "'", "#", " "]
 
 			stripedname = most_rated
 			for c in wrongChar:
 				stripedname = stripedname.replace(c, '')
+			#end crap
 
 			# copy most rated into new folder
 			targetDir = join(self.output_directory, join(stripedname[:1], stripedname[:2], stripedname[:3]).lower())
@@ -117,6 +137,7 @@ class RomFilter:
 			if not self.simulate:
 				copy2(src, targetDir)
 		else:
+			# XXX this could rise a lot of warnings if regions are very restrictive. maybe a supress-warnings or a quiet flag is a good idea
 			self._output( 'Found no rom in ' + path + 'that matches any pattern. Nothing copied.', self.warn)
 		return
 
@@ -131,6 +152,7 @@ def show_help():
 	parser.add_argument('-v', action='count', default=0, help='Increase verbosity level')
 	parser.add_argument('-s', '--simulate', action='store_true', default=False, help="Only output what would hapen but do not actualy copy files or create directories.")
 	parser.add_argument('-e', '--extension', metavar="file_extension", required=True, help="File extension of rom files. Like z64 or smc")
+	parser.add_argument('-r', '--regions', metavar="region_tags", help="Comma seperated list of regions or language ordered by priority.")
 	args = parser.parse_args()
 	#print args
 	return args
